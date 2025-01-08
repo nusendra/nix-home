f8229b2579b04d3448e1d1f9fce5d45ff01aab97
Nusendra Hanggarawan <admin@nusendra.com>
2025-01-08 13:29:48 +0700

    add docker

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -38,6 +38,7 @@
       ANDROID_NDK_HOME="${config.home.homeDirectory}/Library/Android/sdk/ndk";
       JAVA_HOME="/Library/Java/JavaVirtualMachines/zulu-17.jdk/Contents/Home";
       PATH = pkgs.lib.mkBefore "${config.home.homeDirectory}/Library/Android/sdk/emulator:${config.home.homeDirectory}/Library/Android/sdk/platform-tools";
+      NVM_DIR = "$HOME/.nvm";
     };
   };
 
@@ -53,59 +54,66 @@
 
   programs.zsh = {
     enable = true;
-        enableCompletion = false; # enabled in oh-my-zsh
-        shellAliases = {
-          v ="nvim";
-          t ="tmux";
-          work ="cd ~/Projects/";
-          nixconf = "cd ~/.config/nix";
-          gplom = "git pull origin master";
-          gploma = "git pull origin main";
-          gplod = "git pull origin dev";
-          gpsom = "git push origin master";
-          gpsoma = "git push origin main";
-          gpsod = "git push origin dev";
-          gcm = "git checkout master";
-          gcd = "git checkout dev";
-          gs = "git status";
-          gd = "git diff";
-          gst = "git stash";
-          gcb = "git checkout -b ";
-          gc = "git checkout ";
-          glo = "git log --oneline";
-          gl = "git log";
-          gpo = "git push origin ";
-          ga = "git add .";
-          gcom = "git commit -m ";
-          gmer = "git merge ";
+    enableCompletion = false; # enabled in oh-my-zsh
+    shellAliases = {
+      v ="nvim";
+      t ="tmux";
+      work ="cd ~/Projects/";
+      nixconf = "cd ~/.config/nix";
+      gplom = "git pull origin master";
+      gploma = "git pull origin main";
+      gplod = "git pull origin dev";
+      gpsom = "git push origin master";
+      gpsoma = "git push origin main";
+      gpsod = "git push origin dev";
+      gcm = "git checkout master";
+      gcd = "git checkout dev";
+      gs = "git status";
+      gd = "git diff";
+      gst = "git stash";
+      gcb = "git checkout -b ";
+      gc = "git checkout ";
+      glo = "git log --oneline";
+      gl = "git log";
+      gpo = "git push origin ";
+      ga = "git add .";
+      gcom = "git commit -m ";
+      gmer = "git merge ";
 
-          devphp = "cd ~/.config/nix/ && nix develop '.#devShells.php'";
-          devphp81 = "cd ~/.config/nix/ && nix develop '.#devShells.php81'";
-          devjs = "cd ~/.config/nix/ && nix develop '.#devShells.js'";
-          devrust = "cd ~/.config/nix/ && nix develop '.#devShells.rust'";
-          devmariadb = "cd ~/.config/nix/ && nix develop '.#devShells.mariadb'";
-          develasticsearch = "cd ~/.config/nix/ && export NIXPKGS_ALLOW_UNFREE=1 && nix develop --impure '.#devShells.elasticsearch'";
+      devphp = "cd ~/.config/nix/ && nix develop '.#devShells.php'";
+      devphp81 = "cd ~/.config/nix/ && nix develop '.#devShells.php81'";
+      devjs = "cd ~/.config/nix/ && nix develop '.#devShells.js'";
+      devrust = "cd ~/.config/nix/ && nix develop '.#devShells.rust'";
+      devmariadb = "cd ~/.config/nix/ && nix develop '.#devShells.mariadb'";
+      develasticsearch = "cd ~/.config/nix/ && export NIXPKGS_ALLOW_UNFREE=1 && nix develop --impure '.#devShells.elasticsearch'";
 
-          dcb = "docker-compose build";
-          dcu = "docker-compose up";
-          dcud = "docker-compose up -d";
-          dcd = "docker-compose down";
+      dcb = "docker-compose build";
+      dcu = "docker-compose up";
+      dcud = "docker-compose up -d";
+      dcd = "docker-compose down";
 
-          dcbdev = "docker-compose -f docker-compose.dev.yml build";
-          dcudev = "docker-compose -f docker-compose.dev.yml up";
-          dcuddev = "docker-compose -f docker-compose.dev.yml up -d";
-          dcddev = "docker-compose -f docker-compose.dev.yml down";
+      dcbdev = "docker-compose -f docker-compose.dev.yml build";
+      dcudev = "docker-compose -f docker-compose.dev.yml up";
+      dcuddev = "docker-compose -f docker-compose.dev.yml up -d";
+      dcddev = "docker-compose -f docker-compose.dev.yml down";
 
-          dcbprod = "docker-compose -f docker-compose.prod.yml build";
-          dcuprod = "docker-compose -f docker-compose.prod.yml up";
-          dcudprod = "docker-compose -f docker-compose.prod.yml up -d";
-          dcdprod = "docker-compose -f docker-compose.prod.yml down";
-        };
-        oh-my-zsh = {
-          enable = true;
-          plugins = [ "git" ];
-          theme = "robbyrussell";
-        };
+      dcbprod = "docker-compose -f docker-compose.prod.yml build";
+      dcuprod = "docker-compose -f docker-compose.prod.yml up";
+      dcudprod = "docker-compose -f docker-compose.prod.yml up -d";
+      dcdprod = "docker-compose -f docker-compose.prod.yml down";
+    };
+    oh-my-zsh = {
+      enable = true;
+      plugins = [ "git" ];
+      theme = "robbyrussell";
+    };
+    initExtra = ''
+      [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh" # This loads nvm
+      [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" # This loads nvm bash_completion
+
+      export PATH="/Applications/Docker.app/Contents/Resources/bin:$PATH"
+      export DOCKER_HOST=unix:///Users/nusendra/.docker/run/docker.sock
+    '';
   };
 
   # Nicely reload system units when changing configs
