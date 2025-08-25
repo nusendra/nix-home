0d3d613139b1ce8476cebc4eb9936141802f3555
nusendra <admin@nusendra.com>
2025-08-25 21:08:49 +0700

    feat: add keymaps for get file path

--- home-manager/nvim/configs/keymaps.nix
+++ home-manager/nvim/configs/keymaps.nix
@@ -14,7 +14,11 @@
             nnoremap <C-P> :Telescope find_files<CR>
             nnoremap <C-O> :Telescope live_grep<CR>
             nnoremap <C-E> :lua vim.diagnostic.open_float()<CR>
-            nnoremap <C-E> :lua vim.diagnostic.open_float()<CR>
+
+            " Copy current file path to clipboard
+            nnoremap <leader>fp :let @+ = expand('%:p')<CR>:echo "File path copied to clipboard"<CR>
+            nnoremap <leader>fn :let @+ = expand('%:t')<CR>:echo "File name copied to clipboard"<CR>
+            nnoremap <leader>fr :let @+ = expand('%')<CR>:echo "Relative file path copied to clipboard"<CR>
 
             nmap s <Plug>(leap-forward-to)
             nmap S <Plug>(leap-backward-to)
