2bbdc795708fe214581eb3e168560d05b1421635
nusendra <admin@nusendra.com>
2025-11-27 10:44:47 +0700

    feat: add ghostty configuration

---
+++ home-manager/ghostty.nix
@@ -0,0 +1,12 @@
+{
+  programs.ghostty = {
+    enable = true;
+    settings = {
+      theme = "Oceanic Next";
+      font-family = "Monaco";
+      font-size = 12;
+      cursor-style = "block";
+      cursor-opacity = 1;
+    };
+  };
+}
